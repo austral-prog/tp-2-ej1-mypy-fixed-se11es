be6416caceb1ac96eea73e3ddf020b256caf17ea
se11es <162724132+se11es@users.noreply.github.com>
2024-08-29 18:45:37 -0300

    Update maximums.py

--- src/maximums.py
+++ src/maximums.py
@@ -1,13 +1,21 @@
-def max_of_two(x, y):
+def max_of_two(x: float, y: float) -> float:
     """Given x and y, that are 2 numbers, return the biggest number."""
-    biggest = x
     if x >= y:
-        return biggest
+        biggest: float = x
     else:
-        biggest = y
-        return biggest
+        biggest: float = y
+    return biggest
 
-# Replace the "ANSWER HERE" for your answer
-def max_of_three(x, y, z):
-    """Given x, y and z, that are 3 numbers, return the biggest number of the three."""
-    return "ANSWER HERE" # Remove this line and implement
+
+def max_of_three(x: float, y: float, z: float) -> float:
+    """Given x, y, and z, that are 3 numbers, return the biggest number of the three."""
+    
+    biggest: float = x  # Asumimos que x es el mayor inicialmente
+    
+    if y > biggest:
+        biggest = y
+    
+    if z > biggest:
+        biggest = z
+    
+    return biggest
